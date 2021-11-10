2f70da23c337955954155f57a456c36299a3d498
Brandon Fightmaster <that.guy.fight@gmail.com>
2021-11-10 12:49:00 -0500

    Add project files.

---
+++ BlazorApp1/Resources/WeatherForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Resources
+{
+    public class WeatherForm
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Range(0, 99999, ErrorMessage = "Invalid zip code")]
+        public int ZipCode { get; set; } = 0;
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+    }
+}
---
+++ BlazorApp1/Shared/User.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.Shared
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Picture { get; set; }
+        public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? RegisterDate { get; set; }
+        public string? Phone { get; set; }
+        public object? Location { get; set; }
+    }
+}

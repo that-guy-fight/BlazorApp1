b5e06d1685960f796dea6d04c272e2c609f4a07d
brandon.c.fightmaster <brandon.c.fightmaster@saic.com>
2021-12-01 10:37:43 -0500

    updated filtering

---
+++ BlazorApp1/Shared/UserPreview.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Shared
+{
+    public class UserPreview
+    {
+        [Display(Name = "User Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Profile Image")]
+        public string Picture { get; set; }
+    }
+}

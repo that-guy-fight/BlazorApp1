4f857959c5a990b27faf74e521532c533f49e395
Brandon Fightmaster <that.guy.fight@gmail.com>
2021-12-09 11:40:45 -0500

    generate columns and data dynamically

---
+++ BlazorApp1/Shared/SearchFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Shared
+{
+    public class SearchFilter
+    {
+        public SearchFilter(string searchText, bool sortDescending)
+        {
+            SearchText = searchText;
+            SortDescending = sortDescending;
+        }
+        
+        public string SearchText { get; set; }
+
+        public bool SortDescending { get; set; }
+    }
+}
--- BlazorApp1/Shared/UserPreview.cs
+++ BlazorApp1/Shared/UserPreview.cs
@@ -4,19 +4,19 @@ namespace BlazorApp1.Shared
 {
     public class UserPreview
     {
-        [Display(Name = "User Id")]
+        [Display(Name = "User Id", Order = 4)]
         public string Id { get; set; }
 
-        [Display(Name = "Title")]
+        [Display(Name = "Title", Order = 1)]
         public string Title { get; set; }
 
-        [Display(Name = "First Name")]
+        [Display(Name = "First Name", Order = 2)]
         public string FirstName { get; set; }
 
-        [Display(Name = "Last Name")]
+        [Display(Name = "Last Name", Order = 3)]
         public string LastName { get; set; }
 
-        [Display(Name = "Profile Image")]
+        [Display(Name = "Profile Image", Order = 0)]
         public string Picture { get; set; }
     }
 }
